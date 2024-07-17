f991a73c5fddefc8fedc1566465844aeec29f5e6
vishwa <vishwavirat774@gmail.com>
2024-07-17 11:33:02 +0530

    form validations and navigation handling

--- mt-2023/src/components/home/home.js
+++ mt-2023/src/components/home/home.js
@@ -1,91 +1,162 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCountries, updateList } from '../../redux/slices/countrySlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCountries, updateList } from "../../redux/slices/countrySlice";
+
+import GoogleIcon from "../../assets/google.png";
+import TwitterIcon from "../../assets/fb.png";
+import LinkedInIcon from "../../assets/linkedin.png";
+import FbIcon from "../../assets/twitter.png";
 
 const Home = () => {
+  // Variable to handle dispatch
+  const dispatch = useDispatch();
 
-    // Variable to handle dispatch 
-    const dispatch = useDispatch();
+  // Variable to handle countries data
+  const countries = useSelector((state) => state.countries);
 
+  useEffect(() => {
+    dispatch(getCountries());
+  }, []);
 
-    // Variable to handle countries data
-    const countries = useSelector((state) => state.countries)
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState("All");
 
-    useEffect(() => {
-        dispatch(getCountries());
-    }, [])
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    useEffect(() => {
-        console.log("countries", countries)
-    }, [countries])
+  const onFilterChange = (val) => {
+    setSelected(val);
+    dispatch(updateList(val));
+    setIsOpen(false);
+  };
 
-    const [isOpen, setIsOpen] = useState(false);
-    const [selected, setSelected] = useState("All");
+  return (
+    <div className="container vh-100 d-flex flex-column">
+      <nav
+        className={`navbar navbar-expand-md bg-white navbar-dark fixed-top ${
+          isOpen ? "show" : ""
+        }`}
+      >
+        {/* Navbar */}
+        <div class="container">
+          <a href="#" class="navbar-brand text-dark fw-semibold">
+            Countries
+          </a>
+          <button
+            type="button"
+            onClick={toggleMenu}
+            class="navbar-toggler bg-black"
+          >
+            <span class="navbar-toggler-icon" style={{ color: "black" }}></span>
+          </button>
 
-    const toggleMenu = () => {
-        console.log("yuppppppp")
-        setIsOpen(!isOpen);
-    };
-
-    const onFilterChange = (val) =>{
-        setSelected(val);
-        dispatch(updateList(val));
-    }
-
-    const [activePage, setActivePage] = useState("All");
-
-    return <div className='container vh-100 d-flex flex-column'>
-        <nav className={`navbar navbar-expand-md bg-white navbar-dark fixed-top ${isOpen ? "show" : ""
-            }`}>
-
-            {/* Navbar */}
-            <div class="container">
-                <a href="#" class="navbar-brand text-dark fw-semibold">Countries</a>
-                <button
-                    type="button"
-                    onClick={toggleMenu}
-                    class="navbar-toggler bg-dark"
+          <div
+            className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}
+            id="navbarNav"
+          >
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item px-3">
+                <a
+                  href="#"
+                  className={`nav-link text-dark`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onFilterChange("All");
+                  }}
                 >
-                    <span class="navbar-toggler-icon bg-dark"></span>
-                </button>
-
-                <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`} id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item px-3">
-                            <a href="#" className={`nav-link text-dark`} onClick={()=>onFilterChange("All")}>All</a>
-                            {selected=="All" && <div style={{ height: 2, backgroundColor: "grey", width: 40 }}></div>}
-                        </li>
-                        <li className="nav-item px-3">
-                            <a href="#about" className={`nav-link text-dark`} onClick={()=>onFilterChange("Asia")}>Asia</a>
-                            {selected=="Asia" && <div style={{ height: 2, backgroundColor: "grey", width: 50 }}></div>}
-                        </li>
-                        <li className="nav-item px-3">
-                            <a href="#" className={`nav-link text-dark`} onClick={()=>onFilterChange("Europe")}>Europe</a>
-                            {selected=="Europe" && <div style={{ height: 2, backgroundColor: "grey", width: 70 }}></div>}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        {/* List section */}
-        <div className="d-flex flex-wrap pt-30 g-0" style={{ marginTop: 100,gap:10 }}>
-            {countries?.list?.map((item, index) => (
-                <div key={index} className="col d-flex flex-row border border-1 border-black p-2 shadow" style={{minWidth:"49%"}}>
-                    <img
-                        src={item.flag}
-                        className="card-img-left border border-1 rounded-start"
-                        alt="..."
-                        style={{ height: 90, width: 140, objectFit:"cover" }}
-                    />
-                    <div className="d-flex flex-column justify-content-start mt-2 mx-3">
-                        <p className="fw-semibold">{item.name}</p>
-                        <p className="text-dark" style={{marginTop:-15}}>{item.region}</p>
-                    </div>
-                </div>
-            ))}
+                  All
+                </a>
+                {selected == "All" && (
+                  <div
+                    style={{ height: 2, backgroundColor: "grey", width: 40 }}
+                  ></div>
+                )}
+              </li>
+              <li className="nav-item px-3">
+                <a
+                  href="#about"
+                  className={`nav-link text-dark`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onFilterChange("Asia");
+                  }}
+                >
+                  Asia
+                </a>
+                {selected == "Asia" && (
+                  <div
+                    style={{ height: 2, backgroundColor: "grey", width: 50 }}
+                  ></div>
+                )}
+              </li>
+              <li className="nav-item px-3">
+                <a
+                  href="#"
+                  className={`nav-link text-dark`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onFilterChange("Europe");
+                  }}
+                >
+                  Europe
+                </a>
+                {selected == "Europe" && (
+                  <div
+                    style={{ height: 2, backgroundColor: "grey", width: 70 }}
+                  ></div>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
-    </div>
-}
+      </nav>
 
-export default Home
+      {/* List section */}
+      <div
+        className="d-flex flex-wrap pt-30 g-0"
+        style={{ marginTop: 100, gap: 10 }}
+      >
+        {countries?.list?.map((item, index) => (
+          <div
+            key={index}
+            className="col d-flex flex-row border border-1 border-black p-2 shadow"
+            style={{ minWidth: "49%" }}
+          >
+            <img
+              src={item.flag}
+              className="card-img-left border border-1 rounded-start"
+              alt="..."
+              style={{ height: 90, width: 140, objectFit: "cover" }}
+            />
+            <div className="d-flex flex-column justify-content-start mt-2 mx-3">
+              <p className="fw-semibold">{item.name}</p>
+              <p className="text-dark" style={{ marginTop: -15 }}>
+                {item.region}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Footer */}
+      <footer className="mt-5">
+        <div className="d-flex flex-row justify-content-center px-5 mb-3 gap-3 mt-5">
+          <img src={GoogleIcon} style={{ height: 40, width: 40 }} />
+          <img src={FbIcon} style={{ height: 40, width: 40 }} />
+          <img src={LinkedInIcon} style={{ height: 40, width: 40 }} />
+          <img src={TwitterIcon} style={{ height: 40, width: 40 }} />
+        </div>
+
+        <div className="d-flex flex-column align-items-center px-5 mb-3 gap-3 mt-5 mb-5">
+          <h6>Example@gmail.com</h6>
+          <h6 className="text-center">
+            Copyirght © 2020 Name. All rights reserved.
+          </h6>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
--- mt-2023/src/components/signin/signin.js
+++ mt-2023/src/components/signin/signin.js
@@ -1,53 +1,154 @@
-import React from 'react'
-import SigninStyle from './signin.module.css'
+import React from "react";
+import SigninStyle from "./signin.module.css";
+import { useNavigate } from "react-router-dom";
 
-import GoogleIcon from '../../assets/google.png'
-import TwitterIcon from '../../assets/fb.png'
-import LinkedInIcon from '../../assets/linkedin.png'
-import FbIcon from '../../assets/twitter.png'
-import Illustation from '../../assets/Illustration.png'
+import GoogleIcon from "../../assets/google.png";
+import TwitterIcon from "../../assets/fb.png";
+import LinkedInIcon from "../../assets/linkedin.png";
+import FbIcon from "../../assets/twitter.png";
+import Illustation from "../../assets/Illustration.png";
+
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const Signin = () => {
-    return <div className='container d-flex justify-content-center align-items-center vh-100'>
-        {/* Left View */}
-        <div className='col-sm-12 col-lg-6 d-flex flex-column justify-content-end'>
+  // Variable to handle navigation
+  const navigation = useNavigate();
+
+  // Variable to handle validation schema
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters long")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+        "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special symbol"
+      )
+      .required("Password is required"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ email: "", password: "", checkboxValue: false }}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        // Handle form submission logic here
+        navigation("/home");
+        console.log("Submitted values:", values);
+      }}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        values,
+        errors,
+        touched,
+        isValid,
+        handleSubmit,
+      }) => (
+        <div className="container d-flex justify-content-center align-items-center vh-100">
+          {/* Left View */}
+          <div className="col-sm-12 col-lg-6 d-flex justify-content-end">
             {/* <div className='d-flex flex-column justify-self-end'>yupp</div> */}
-            <h3>Sign In</h3>
-            <p className='mt-1'>New user? <span className='text-primary'>create an account</span></p>
-            <div className={`${SigninStyle.signinContainer} d-flex flex-column w-400`}>
+            <div>
+              <h3>Sign In</h3>
+              <p className="mt-1">
+                New user?{" "}
+                <span className="text-primary">create an account</span>
+              </p>
+              <div
+                className={`${SigninStyle.signinContainer} d-flex flex-column w-400`}
+              >
                 {/* <input type="email" class="form-control" placeholder='Username or email' className='mb-2 p-2' />
                 <input type="password" class="form-control" placeholder='Password' className='mt-1 p-2' /> */}
-                <input type="email" className={`form-control mb-2 p-2 border border-1 border-dark`} placeholder="Username or email" />
-                <input type="password" className={`form-control mt-1 p-2 border border-1 border-dark`} placeholder="Password" />
+                <input
+                  type="email"
+                  className={`form-control mb-2 p-2 border border-1 border-dark`}
+                  placeholder="Username or email"
+                  value={values.email}
+                  onChange={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                />
+                {errors.email && touched.email && (
+                  <div className="text-danger mb-1">{errors.email}</div>
+                )}
+                <input
+                  type="password"
+                  className={`form-control mt-1 p-2 border border-1 border-dark`}
+                  placeholder="Password"
+                  value={values.password}
+                  onChange={handleChange("password")}
+                  onBlur={handleBlur("passwword")}
+                />
+                {errors.password && (
+                  <div className="text-danger mb-1">{errors.password}</div>
+                )}
 
                 <div class="form-check mt-3 d-flex align-items-center">
-                    <input type="checkbox" className={`${SigninStyle.checkboxstyle} form-check-input`} />
-                    <span className='mx-3'>Keep me signed in</span>
+                  <input
+                    type="checkbox"
+                    value={values.checkboxValue}
+                    onChange={handleChange("checkboxValue")}
+                    onBlur={handleBlur("checkboxValue")}
+                    className={`${SigninStyle.checkboxstyle} form-check-input`}
+                  />
+                  <span className="mx-3">Keep me signed in</span>
                 </div>
-                <button className='bg-dark text-white py-2 mt-3'>Sign In</button>
+                <button
+                  className="bg-dark text-white py-2 mt-3"
+                  disabled={!isValid}
+                  type="submit"
+                  onClick={handleSubmit}
+                >
+                  Sign In
+                </button>
 
                 {/* Social Signin */}
-                <div className='d-flex flex-row mt-5 align-items-center'>
-                    <div style={{width:"30%",height:2,backgroundColor:"grey",opacity:0.5}}></div>
-                    <span className='d-flex justify-content-center d-inline' style={{width:"45%"}}>Or Sign In With</span>
-                    <div style={{width:"30%",height:2,backgroundColor:"grey",opacity:0.5}}></div>
+                <div className="d-flex flex-row mt-5 align-items-center">
+                  <div
+                    style={{
+                      width: "30%",
+                      height: 2,
+                      backgroundColor: "grey",
+                      opacity: 0.5,
+                    }}
+                  ></div>
+                  <span
+                    className="d-flex justify-content-center d-inline"
+                    style={{ width: "45%" }}
+                  >
+                    Or Sign In With
+                  </span>
+                  <div
+                    style={{
+                      width: "30%",
+                      height: 2,
+                      backgroundColor: "grey",
+                      opacity: 0.5,
+                    }}
+                  ></div>
                 </div>
 
-                <div className='d-flex flex-row justify-content-around px-5 mt-4'>
-                    <img src={GoogleIcon} style={{height:40,width:40}}/>
-                    <img src={FbIcon} style={{height:40,width:40}}/>
-                    <img src={LinkedInIcon} style={{height:40,width:40}}/>
-                    <img src={TwitterIcon} style={{height:40,width:40}}/>
+                <div className="d-flex flex-row justify-content-around px-5 mt-4">
+                  <img src={GoogleIcon} style={{ height: 40, width: 40 }} />
+                  <img src={FbIcon} style={{ height: 40, width: 40 }} />
+                  <img src={LinkedInIcon} style={{ height: 40, width: 40 }} />
+                  <img src={TwitterIcon} style={{ height: 40, width: 40 }} />
                 </div>
-
+              </div>
             </div>
-        </div>
+          </div>
 
-        {/* Right View */}
-        <div className='d-none col-sm-12 col-lg-6 d-lg-flex justify-content-center'>
-            <img src={Illustation} style={{height:"100%",width:230}}/>
+          {/* Right View */}
+          <div className="d-none col-sm-12 col-lg-6 d-lg-flex justify-content-center">
+            <img src={Illustation} style={{ height: "100%", width: 230 }} />
+          </div>
         </div>
-    </div>
-}
+      )}
+    </Formik>
+  );
+};
 
-export default Signin
+export default Signin;
